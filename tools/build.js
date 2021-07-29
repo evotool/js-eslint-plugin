1a2a97784adcd3dab5946fce55950c79ab1b3df2
Shamil Sultanov <shamil.amirovich@gmail.com>
2021-07-30 01:31:25 +0300

    feat: update build.js

--- tools/build.js
+++ tools/build.js
@@ -5,8 +5,6 @@ const { resolve } = require('path');
 const packageJson = JSON.parse(readFileSync(resolve(__dirname, '../package.json')).toString());
 
 delete packageJson.scripts;
-delete packageJson.jest;
-delete packageJson.nodemonConfig;
 delete packageJson.devDependencies;
 
 writeFileSync(resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson, null, '\t'));
@@ -15,10 +13,4 @@ const jsTsFiles = sync(resolve(__dirname, '../dist/**/*.{js,ts}'));
 
 for (const f of jsTsFiles) {
 	writeFileSync(f, readFileSync(f, 'utf-8').replace(/ {4}/g, '\t'));
-}
-
-const copyingFiles = ['LICENSE', 'README.md'];
-
-for (const cf of copyingFiles) {
-	copyFileSync(resolve(__dirname, `../${cf}`), resolve(__dirname, `../dist/${cf}`));
 }
