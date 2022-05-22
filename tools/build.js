da82734a38fcf698b57014de4470f4af065c0cef
Shamil Sultanov <shamil.amirovich@gmail.com>
2022-05-22 16:51:42 +0300

    patch: add README.md to dist

--- tools/build.js
+++ tools/build.js
@@ -7,6 +7,7 @@ const packageJson = JSON.parse(readFileSync(resolve(__dirname, '../package.json'
 delete packageJson.scripts;
 delete packageJson.devDependencies;
 
+copyFileSync(resolve(__dirname, '../README.md'), resolve(__dirname, '../dist/README.md'));
 writeFileSync(resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson, null, '\t'));
 
 const jsTsFiles = sync(resolve(__dirname, '../dist/**/*.{js,ts}'));
